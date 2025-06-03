8cab6bf65c730979a5295e3e1ac41a440dc39d09
vaibhav3792 <vaibhavgupta8807@gmail.com>
2025-06-03 14:31:09 +0530

    Added Task 2

---
+++ Task2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2;
+    char op;
+
+    cout << "Basic Calculator Program" << endl;
+    cout << "------------------------" << endl;
+
+    // Input two numbers
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Choose operation
+    cout << "Choose operation (+, -, *, /): ";
+    cin >> op;
+
+    double result;
+
+    // Perform calculation based on operator
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '*':
+            result = num1 * num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                cout << "Result: " << result << endl;
+            } else {
+                cout << "Error: Division by zero is not allowed." << endl;
+            }
+            break;
+        default:
+            cout << "Invalid operator. Please choose +, -, *, or /." << endl;
+    }
+
+    return 0;
+}
